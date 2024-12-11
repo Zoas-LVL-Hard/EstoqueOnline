aaeef008d599bd60f76d77f285dfb0a20e5307b3
Victor <victormota_03@hotmail.com>
2024-12-11 20:38:48 -0300

    Primeiro Commit

---
+++ src/main/java/com/center/stock/estoque_online/EstoqueOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.center.stock.estoque_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueOnlineApplication.class, args);
+	}
+
+}
